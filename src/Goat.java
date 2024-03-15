bcc714f04c2a6e45ccf15a2d201f463ebb0137b5
Filipp <restokov@yandex.ru>
2024-03-15 03:06:36 +0300

    added car class

--- src/Goat.java
+++ src/Goat.java
@@ -5,35 +5,42 @@ public class Goat {
     private Integer age;
 
     public Goat(String name, String gender, Integer age) throws Exception {
+        this.setName(name);
+        this.setGender(gender);
+        this.setAge(age);
+    }
+
+    private void setName(String name) throws Exception {
         if (name.length() > 3) {
             this.name = name;
         } else {
             throw new Exception("Имя короче 3 символов");
         }
+    }
 
-        if (gender.toLowerCase().equals("male") || gender.toLowerCase().equals("female")) {
+    private void setGender(String gender) throws Exception {
+        if (gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female")) {
             this.gender = gender;
         } else {
             throw new Exception("Неверный пол");
         }
+    }
 
+    private void setAge(Integer age) throws Exception {
         if (age >= 0) {
             this.age = age;
         } else {
             throw new Exception("Возраст не может быть меньше 0");
         }
-
     }
 
     private int getProductivity() {
         int prod = 0;
-        if (gender.toLowerCase().equals("female")) {
-            if (age < 1) {
-                prod = 0;
+        if (gender.equalsIgnoreCase("female")) {
+            if (age >= 1 && age <= 6) {
+                prod = 5;
             } else if (age > 6) {
                 prod = 2;
-            } else {
-                prod = 5;
             }
         }
         return prod;
